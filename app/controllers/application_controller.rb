05adb65c990fa9010e1b1998e147e976a51416c2
ankitchauhan2065 <ankitchauhan2065@gmail.com>
2018-04-16 15:07:34 +0200

    add devise and bootstrap for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user! #we need a authenticated user to perform any action my application contoller
 end
